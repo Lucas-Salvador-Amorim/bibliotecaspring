f45672eaa1e0eea6ed00b0119c49f040b06c8b5e
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2023-05-09 17:47:51 +0000

    insert Livro

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.LivroRepository;
+import application.model.Livro;
 
 @Controller
 public class LivroController {
@@ -18,4 +21,20 @@ public class LivroController {
         model.addAttribute("livros", livroRepo.findAll());
         return "WEB-INF/list.jsp";
     }
+
+    @RequestMapping("/insert")
+    public String insert(){
+        return "WEB-INF/insert.jsp";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("titulo") String titulo){
+        
+       Livro livro = new Livro();
+       livro.setTitulo(titulo);
+
+        livroRepo.save(livro);         
+        return "redirect:/livro";
+    }
+
 }
